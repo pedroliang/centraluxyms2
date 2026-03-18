a5e012a401ec20a12df736eab89e1e01548cfc7a
Luciano <pedro.liang@gmail.com>
2026-03-18 17:17:52 -0300

    fix: configure base path and github actions deploy

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,9 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/centraluxyms2/",
   server: {
+
     host: "::",
     port: 8080,
     hmr: {
